ba3f8475c9931b9a90c7213f8b5f5533a0a8a214
RoGgY <roggy.work@gmail.com>
2026-03-25 01:44:18 +0400

    LinkedList: Added support of ICollection<T>

--- ComputerScience.DataStructures/MyLinkedList.cs
+++ ComputerScience.DataStructures/MyLinkedList.cs
@@ -2,7 +2,7 @@
 
 namespace ComputerScience.DataStructures
 {
-    public class MyLinkedList<T> : IEnumerable<T>, ICollection<T>, IList<T>
+    public class MyLinkedList<T> : ICollection<T>, IList<T>
     {
 
         private Node? first;
@@ -24,7 +24,7 @@ namespace ComputerScience.DataStructures
             get => last != null ? last.Data : throw new NullReferenceException();
             set
             {
-                if (last == null || value == null)
+                if (last == null)
                 {
                     throw new NullReferenceException();
                 }
@@ -48,14 +48,11 @@ namespace ComputerScience.DataStructures
             else if (length == 1)
             {
                 first = new Node(data: default);
-                First = default;
-                Last = First;
                 last = first;
             }
             else
             {
                 first = new Node(data: default);
-                First = default;
                 Node current = first;
                 Node next;
                 for (int i = 1; i < length; i++)
@@ -68,6 +65,59 @@ namespace ComputerScience.DataStructures
                 last = current;
             }
             Count = length;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                if (index < Count / 2)
+                {
+                    Node current = first!;
+                    for (int i = 0; i < index; i++)
+                    {
+                        current = current.Next!;
+                    }
+                    return current.Data ?? throw new NullReferenceException();
+                }
+                else
+                {
+                    Node current = last!;
+                    for (int i = Count - 1; i > index; i--)
+                    {
+                        current = current.Previous!;
+                    }
+                    return current.Data ?? throw new NullReferenceException();
+                }
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                if (index < Count / 2)
+                {
+                    Node current = first!;
+                    for (int i = 0; i < index; i++)
+                    {
+                        current = current.Next!;
+                    }
+                    current.Data = value;
+                }
+                else
+                {
+                    Node current = last!;
+                    for (int i = Count-1; i>index; i--)
+                    {
+                        current = current.Previous!;
+                    }
+                    current.Data = value;
+                }
+            }
         }
 
         public void Add(T value)
@@ -125,6 +175,67 @@ namespace ComputerScience.DataStructures
             Count++;
         }
 
+        public void Clear()
+        {
+            if (Count == 0)
+            {
+                return;
+            }
+            else if (Count == 1)
+            {
+                first = null;
+                last = null;
+                return;
+            }
+            Node current = first!;
+            for (int i = 0; i < Count - 1; i++)
+            {
+                current = current.Next!;
+                current.Previous = null;
+            }
+            if (last != null)
+            {
+                last = null;
+            }
+            Count = 0;
+        }
+
+        public void CopyTo (T[] target, int index)
+        {
+            if (target == null)
+            {
+                throw new ArgumentNullException("Target array is null!");
+            }
+            else if (target.Length < Count)
+            {
+                throw new ArgumentOutOfRangeException("Array length is less than length of list!");
+            }
+            Node? current = first;
+            for (int i = 0; i < Count; i++)
+            {
+                target[i+index] = current!.Data!;
+                current = current.Next;
+            }
+        }
+
+        public bool Contains (T value)
+        {
+            Node? current = first;
+            for (int i = 0; i < Count; i++)
+            {
+                if (current != null)
+                {
+                    if (current.Data!=null
+                    &&current.Data.Equals(value))
+                    {
+                        return true;
+                    }
+                    current=current.Next;
+                }
+            }
+            return false;
+        }
+
         public bool Remove(T value)
         {
             if (Count == 0)
@@ -163,6 +274,17 @@ namespace ComputerScience.DataStructures
             return false;
         }
 
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node? current = first;
+            while (current != null)
+            {
+                yield return current.Data!;
+                current = current.Next;
+            }
+        }
         private Node GetNodeByIndex(int index)
         {
             if (index < 0 || index >= Count)
@@ -186,83 +308,6 @@ namespace ComputerScience.DataStructures
                     current = current.Previous!;
                 }
                 return current;
-            }
-        }
-
-        public void Clear()
-        {
-            if (Count == 0)
-            {
-                return;
-            }
-            else if (Count == 1)
-            {
-                first = null;
-                last = null;
-                return;
-            }
-            Node current = first!;
-            for (int i = 0; i < Count - 1; i++)
-            {
-                current = current.Next!;
-                current.Previous = null;
-            }
-            if (last != null)
-            {
-                last = null;
-            }
-            Count = 0;
-        }
-
-        public T this[int index]
-        {
-            get
-            {
-                if (index < 0 || index >= Count)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-                if (index < Count / 2)
-                {
-                    Node current = first!;
-                    for (int i = 0; i < index; i++)
-                    {
-                        current = current.Next!;
-                    }
-                    return current.Data ?? throw new NullReferenceException();
-                }
-                else
-                {
-                    Node current = last!;
-                    for (int i = Count - 1; i > index; i--)
-                    {
-                        current = current.Previous!;
-                    }
-                    return current.Data ?? throw new NullReferenceException();
-                }
-            }
-            set
-            {
-                if (index < 0 || index >= Count)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-                Node current = first!;
-                for (int i = 0; i < index; i++)
-                {
-                    current = current.Next!;
-                }
-                current.Data = value;
-            }
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            Node? current = first;
-            while (current != null)
-            {
-                yield return current.Data!;
-                current = current.Next;
             }
         }
 
